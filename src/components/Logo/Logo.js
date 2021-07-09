e1bb935eb85a18a05dae539d9a40358a6a0b1321
Gregory Lustiber <glustiber@gmail.com>
2021-07-09 16:26:06 -0600

    Added Navigation, Logo, and ImageLinkForm components

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import brain from './brain.png';
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner">
+                    <img alt='Logo' src={brain} style={{paddingTop:'25px'}}/>
+                </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
