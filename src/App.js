42817054ec5be0d94a7ff188ff7a88662940b7b6
Gregory Lustiber <glustiber@gmail.com>
2021-07-09 22:25:47 -0600

    Added Face Box, Register, Sign In, Routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import './App.css';
@@ -30,8 +32,31 @@ class App extends Component {
         super();
         this.state = {
             input: '',
-            imageUrl: ''
+            imageUrl: '',
+            box: {},
+            route: 'signIn',
+            isSignedIn: false
         }
+    }
+
+    calculateFaceLocation = (data) => {
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+        const image = document.getElementById('inputimage');
+        const width = Number(image.width);
+        const height = Number(image.height);
+        // Want to return object which will fill up box state
+        // Need to figure out top left, top right, bottom right, bottom left dots
+        return {
+            leftCol: clarifaiFace.left_col * width,
+            topRow: clarifaiFace.top_row * height,
+            rightCol: width - (clarifaiFace.right_col * width),
+            bottomRow: height - (clarifaiFace.bottom_row * height)
+        }
+    }
+
+    displayFaceBox = (box) => {
+        this.setState({box: box});  // can do ({box}) with ES6
+        console.log(box);
     }
 
     /* Anytime there's an event listener, we receive "event" */
@@ -44,14 +69,21 @@ class App extends Component {
 
         app.models.predict(
             Clarifai.FACE_DETECT_MODEL, 
-            this.state.input).then(
-            function (response) {
-                console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-            },
-            function (err) {
+            this.state.input)
+            // Must use this. bc using classes here
+            // calculateFaceLocation takes response and returns box object
+            // That object is now going into displayFaceBox
+            .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+            .catch(err => console.log(err));
+    }
 
-            }
-        );
+    onRouteChange = (route) => {
+        if (route === 'signOut') {
+            this.setState({ isSignedIn: false })
+        } else if (route === 'home') {
+            this.setState({ isSignedIn: true })
+        }
+        this.setState({route: route});
     }
 
     render() {
@@ -60,12 +92,22 @@ class App extends Component {
                 <Particles 
                     className="particles" 
                     params={particlesOptions} />
-            <Navigation />
-            <Logo />
-            <Rank />
-            {/* onInputChange passed as prop. B/c its part of this class, need to use "this" to access bc onInputChange is property of App */}
-            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-            <FaceRecognition imageUrl={this.state.imageUrl}/>
+            <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+            { this.state.route === 'home'
+                ? <div>
+                    <Logo />
+                    <Rank />
+                    {/* onInputChange passed as prop. B/c its part of this class, need to use "this" to access bc onInputChange is property of App */}
+                    <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+                    <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+                </div>
+                : (
+                    this.state.route === 'signIn' 
+                    ? <SignIn onRouteChange={this.onRouteChange} />
+                    : <Register onRouteChange={this.onRouteChange} />
+                )
+                     
+            }
             </div>
         );
     }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
     return (
         <div className='center ma'>
-            <div className='absolue mt2'>
-                <img src={imageUrl} alt='detect' width='500px' height='auto' />
+            <div className='absolute mt2'>
+                <img id='inputimage' src={imageUrl} alt='' width='500px' height='auto' />
+                <div class='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
     );
